bade33c5654d7e58d92c32f479a1cb61838a47ab
burns115 <rburns@email.neit.edu>
2024-02-06 17:22:25 -0500

    Add project files.

---
+++ BlockBuster/BlockBusterBasicFunctions.cs
@@ -0,0 +1,56 @@
+﻿using BlockBuster.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockBuster
+{
+    public class BlockBusterBasicFunctions
+    {
+        public static Movie GetMovieById(int id)
+        {
+            using (var db = new SE407_BlockBusterContext())
+            {
+                return db.Movies.Find(id);
+            }
+        }
+
+        public static List<Movie> GetAllMovies()
+        {
+            using (var db = new SE407_BlockBusterContext())
+            {
+                return db.Movies.ToList();
+            }
+        }
+
+        public static List<Movie> GetAllCheckedOutMovies()
+        {
+            using (var db = new SE407_BlockBusterContext())
+            {
+                return db.Movies
+                    .Join(db.Transactions,
+                    m => m.MovieId,
+                    t => t.Movie.MovieId,
+                    (m, t) => new
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        DirectorId = m.DirectorId,
+                        CheckedIn = t.CheckedIn
+                    }).Where(w => w.CheckedIn == "N")
+                    .Select(m => new Movie
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        DirectorId = m.DirectorId
+                    }).ToList();
+            }
+        }
+    }
+}
---
+++ BlockBusterTest/BlockBusterBasicFunctionsTest.cs
@@ -0,0 +1,38 @@
+﻿using BlockBuster;
+using BlockBuster.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BlockBusterTest
+{
+    public class BlockBusterBasicFunctionsTest
+    {
+        [Fact]
+        public void GetMovieByIdTest()
+        {
+            var result = BlockBusterBasicFunctions.GetMovieById(11);
+            Assert.True(result.Title == "Vertigo");
+            Assert.True(result.ReleaseYear == 1958);
+        }
+
+        [Fact]
+        public void GetAllMovies()
+        {
+            var result = BlockBusterBasicFunctions.GetAllMovies();
+            Assert.True(result.Count == 50);
+        }
+
+        [Fact]
+        public void GetAllCheckedOutMovies()
+        {
+            var result = BlockBusterBasicFunctions.GetAllCheckedOutMovies();
+            Assert.True(result.Count == 3);
+        }
+    }
+
+    
+}
